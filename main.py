3a33780fc52060ae6f226634a1d7751634ccb5fc
taufiqdp <topexdw@gmail.com>
2025-04-05 15:13:17 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+
+from agno.agent import Agent
+from agno.models.azure.ai_foundry import AzureAIFoundry
+from agno.tools.duckduckgo import DuckDuckGoTools
+from dotenv import load_dotenv
+
+load_dotenv()
+azure_endpoint = os.getenv("AZURE_ENDPOINT")
+api_key = os.getenv("AZURE_API_KEY")
+api_version = os.getenv("AZURE_API_VERSION")
+
+
+agent = Agent(
+    model=AzureAIFoundry(id="DeepSeek-R1"),
+    tools=[DuckDuckGoTools()],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+agent.print_response("Whats happening in France?", stream=True)
